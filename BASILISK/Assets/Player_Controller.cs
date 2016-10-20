8d6f5cdff423ec6d000f47ca8f4948102ccfa31e
Kawdek <rexg@uci.edu>
2016-10-19 19:16:09 -0700

    Movement Progress
    More generalization + test of different movement speeds

--- BASILISK/Assets/Player_Controller.cs
+++ BASILISK/Assets/Player_Controller.cs
@@ -2,10 +2,16 @@
 using System.Collections;
 
 public class Player_Controller : MonoBehaviour {
-    int floorMask;
+    
+	public float speedMultiplier;
+
+	int floorMask;
     Rigidbody player_rb;
     // Use this for initialization
+	void Start () {
+	}
     void Awake () {
+		speedMultiplier = 2f;
         floorMask = LayerMask.GetMask("Floor");
         player_rb = GetComponent<Rigidbody>();
 	}
@@ -35,10 +41,25 @@ public class Player_Controller : MonoBehaviour {
 
     }
 	void Update () {
-		var mov_V = Input.GetAxis("Vertical") * Time.deltaTime * 2.0f;
-		var mov_H = Input.GetAxis("Horizontal") * Time.deltaTime * 2.0f;
+		var mov_V = Input.GetAxis("Vertical") * getSpeed();
+		var mov_H = Input.GetAxis("Horizontal") * getSpeed();
 
 		transform.Translate(0, 0, mov_V, Space.World);
 		transform.Translate(mov_H, 0, 0, Space.World);
+	}
+
+	private float getSpeed () {
+		float speed = Time.deltaTime;
+		speed *= speedMultiplier;
+		speed *= speedKeys();
+		return speed;
+	}
+	private float speedKeys () {
+		if (Input.GetAxis ("Run") > 0)
+			return 2.0f;
+		else if (Input.GetAxis ("Run") < 0)
+			return 0.5f;
+		else
+			return 1f;
 	}
 }
