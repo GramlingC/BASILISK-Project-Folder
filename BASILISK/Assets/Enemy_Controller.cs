fb598b8fd84cdc44486ee1ef3a0c6490327e832e
Kaitlyn Deppmeier <kdeppmeier@yahoo.com>
2016-10-20 21:15:24 -0700

    Small improvements for Enemy_Controller

--- BASILISK/Assets/Enemy_Controller.cs
+++ BASILISK/Assets/Enemy_Controller.cs
@@ -11,14 +11,19 @@ public class Enemy_Controller : MonoBehaviour
 
     public Vector3[] coords; //List of coordinates the enemy will travel to, in order.
     private int nextCoord;  //Index in coords of the next coordinate the enemy will pass through.
+    private float yOffset;  //y coordinate the enemy starts at.  This is used to keep the enemy's y coordinate constant.
 
     // Use this for initialization
     void Start ()
     {
-	    //Sets nextCoord to element 1 of coords (0 is the starting position)
+        yOffset = transform.position.y;
+
+        //Sets nextCoord to element 1 of coords (0 is the starting position)
         if (coords.Length > 1)
             nextCoord = 1;
         enemySpeed = .05F;
+
+        //May add some code to ensure that the enemy's orignal position is included in the set of coordiantes.
 	}
 	
 	// Update is called once per frame
@@ -28,12 +33,19 @@ public class Enemy_Controller : MonoBehaviour
         //  Vector3 Enemy = new Vector3(-0.5f, 0, 0);
         //    rb.AddForce(Enemy);
 
-        //In case the enemy just stands in one place.
+        //Preliminary work for detecting player.  Need to figure out if the enemy can see from all directions or just the front.
+        //RaycastHit hit;
+        /*if(Physics.Raycast(transform.position, Vector3.forward, out hit, 1.5F))
+        {
+            //Debug.Log(hit.transform.gameObject.tag);
+            if (hit.transform.gameObject.tag == "Player")
+                RouteEnemy(hit.transform.position);
+        }*/
+
         if (coords.Length > 1)
         {
             //Moves the enemy towards the next point.
-            transform.position = Vector3.MoveTowards(transform.position, coords[nextCoord], enemySpeed*speedMultiplier);
-            //Possibly temporary, as this does not deal with obstacles.
+            RouteEnemy(coords[nextCoord]);
 
             //If enemy has reached nextCoord, it updates the next coordinate index so the enemy changes direction.
             if (transform.position == coords[nextCoord])
@@ -44,13 +56,29 @@ public class Enemy_Controller : MonoBehaviour
                     nextCoord = 0;
             }
         }
+        else if (coords.Length == 1 && transform.position != coords[0])
+        {
+            //If the enemy's "patrol" is sitting in one place, they are returned to that place after chasing.
+            //If there are no coordinates, the enemy won't return to their original position.
+            RouteEnemy(coords[0]);
+        }
 	}
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag=="Light_Hitbox")
         {
+            Debug.Log("Enemy hit by light");
             Destroy(gameObject);
         }
         //Having the Light_Hitbox check for enemies may be better for the framerate.
+    }
+
+    //Routes the enemy to point dest.
+    private void RouteEnemy(Vector3 dest)
+    {
+        //Will likely be changed to allow routing around obstacles.
+        //When sprites are added, code to change the faced direction may go here.
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(dest.x, yOffset, dest.z), enemySpeed * speedMultiplier);
     }
 }
