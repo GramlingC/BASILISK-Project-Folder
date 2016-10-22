71867e1b26901becb50071a5a96b572ba40acd87
Christian Gramling <gramlinc@uci.edu>
2016-10-21 23:07:02 -0700

    Prototype CastLight Method
    Created a prototype raycasting method that triggers enemies. Added the
    "Enemy" tag

--- BASILISK/Assets/Player_Controller.cs
+++ BASILISK/Assets/Player_Controller.cs
@@ -7,6 +7,7 @@ public class Player_Controller : MonoBehaviour {
 
 	int floorMask;
     Rigidbody player_rb;
+    Light player_light;
     // Use this for initialization
 	void Start () {
 	}
@@ -14,6 +15,7 @@ public class Player_Controller : MonoBehaviour {
 		speedMultiplier = 2f;
         floorMask = LayerMask.GetMask("Floor");
         player_rb = GetComponent<Rigidbody>();
+        player_light = GetComponentInChildren<Light>();
 	}
     void FixedUpdate()
     {
@@ -40,12 +42,42 @@ public class Player_Controller : MonoBehaviour {
         }
 
     }
+    //Prototype method for player light casting: Raycasts in a fan, Raycasts trigger enemy behavior
+    //It doesn't seem to be working well, though...
+    void CastLight ()
+    {
+        //To simplify later code
+        float angle_radius = player_light.spotAngle / 2;
+        float angle_increment = player_light.spotAngle / 10;
+        float light_length = player_light.range;
+        //Iterate through angles
+        for (float degree = -angle_radius; degree < angle_radius; degree += angle_increment)
+        {
+            //Code to calculate direction based on degree and Vector3.forward
+            Vector3 LightDirection = Quaternion.AngleAxis(degree, Vector3.up) * transform.forward;
+
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position, LightDirection, out hit, light_length))
+            {
+                //Debug.Log(hit.transform.gameObject.tag);
+                if (hit.transform.gameObject.tag == "Enemy")
+                {
+                    //Trigger enemy behavior
+                    Debug.Log(hit.transform.gameObject.tag);
+                }
+            }
+        }
+
+    }
 	void Update () {
 		var mov_V = Input.GetAxis("Vertical") * getSpeed();
 		var mov_H = Input.GetAxis("Horizontal") * getSpeed();
 
 		transform.Translate(0, 0, mov_V, Space.World);
 		transform.Translate(mov_H, 0, 0, Space.World);
+
+        //Uncomment method call if you get it to work
+        //CastLight();
 	}
 
 	private float getSpeed () {
