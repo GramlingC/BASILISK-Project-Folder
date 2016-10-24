d881b19e61ea5c4e914ae5bcdfe81ca8fffac083
Remenes <yej9719@yahoo.com>
2016-10-24 10:39:15 -0700

    Made light diminish over time

--- BASILISK/Assets/Scripts/Player_Controller.cs
+++ BASILISK/Assets/Scripts/Player_Controller.cs
@@ -9,8 +9,9 @@ public class Player_Controller : MonoBehaviour {
 	int floorMask;
     Rigidbody player_rb;
     Light player_light;
+    public float lightDiminishPerSecond = 5f;
     // Use this for initialization
-	void Start () {
+    void Start () {
 	}
     void Awake () {
 		speedMultiplier = 2f;
@@ -21,6 +22,37 @@ public class Player_Controller : MonoBehaviour {
     void FixedUpdate()
     {
         Turning();
+    }
+    void Update() {
+        var mov_V = Input.GetAxis("Vertical") * getSpeed();
+        var mov_H = Input.GetAxis("Horizontal") * getSpeed();
+
+        transform.Translate(0, 0, mov_V, Space.World);
+        transform.Translate(mov_H, 0, 0, Space.World);
+
+        //Call CastLight
+        CastLight();
+        gradualLightDiminish();
+    }
+
+    //Functions dealing wih diminishing light. Public functions are so that other scripts can call it also
+    public void diminishLight(float angleAmount) {
+        player_light.spotAngle -= angleAmount;
+    }
+
+    private void gradualLightDiminish() {
+        if (player_light.spotAngle > 1)
+            diminishLight(lightDiminishPerSecond * Time.deltaTime);
+        disableLight();
+    }
+
+    //Light's spotAngle can't go lower than 1, so if it is 1, then turn the light object off
+    private void disableLight() {
+        player_light.gameObject.SetActive(player_light.spotAngle <= 1 ? false : true);
+    }
+
+    public bool lightIsDisabled() {
+        return !player_light.gameObject.activeSelf;
     }
 	
 	// Update is called once per frame
@@ -47,6 +79,9 @@ public class Player_Controller : MonoBehaviour {
     //It doesn't seem to be working well, though...
     void CastLight ()
     {
+        //Only when light is enabled
+        if (lightIsDisabled())
+            return;
         //To simplify later code
         float angle_radius = player_light.spotAngle / 2;
         float angle_increment = player_light.spotAngle / numberOfRays;
@@ -74,16 +109,6 @@ public class Player_Controller : MonoBehaviour {
         }
 
     }
-	void Update () {
-		var mov_V = Input.GetAxis("Vertical") * getSpeed();
-		var mov_H = Input.GetAxis("Horizontal") * getSpeed();
-
-		transform.Translate(0, 0, mov_V, Space.World);
-		transform.Translate(mov_H, 0, 0, Space.World);
-
-        //Call CastLight
-        CastLight();
-	}
 
 	private float getSpeed () {
 		float speed = Time.deltaTime;
