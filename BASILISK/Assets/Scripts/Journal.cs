f2557445e4909f8e3fff79f55bf27ecc4b7a4f9a
Carnegie-senpai <sawyer3nick@gmail.com>
2016-10-27 19:53:45 -0700

    Added a Journal count, and the ability to pick up journals

---
+++ BASILISK/Assets/Scripts/Journal.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Journal : MonoBehaviour {
+
+    public GameObject player;
+    public int Journal_Count;
+   
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var distance_to_journal = Vector3.Distance(player.transform.position, transform.position);
+        if (distance_to_journal < 2 && Input.GetKeyDown(KeyCode.E))
+        {
+            Journal_Count = Journal_Count + 1;
+            transform.Translate(0, -10, 0);
+        }
+
+    }
+    void OnGUI() {
+        var distance_to_journal = Vector3.Distance(player.transform.position, transform.position);
+        if (distance_to_journal < 2)
+        {
+            GUI.Label(new Rect(10, 10, 100, 200), "Press 'E' to pick up");
+        }
+        GUI.Label(new Rect(200, 10, 100, 200), "Journal Count:");
+        GUI.Label(new Rect(286, 11f, 100, 200), Journal_Count.ToString());
+    }
+}
