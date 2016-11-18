bb96681b87f44e7487108ba4de22e40fe26a7392
Carnegie-senpai <sawyer3nick@gmail.com>
2016-11-18 10:17:26 -0800

    Continuing work on Scene saving
    Still broken

--- BASILISK/Assets/Scripts/Game_Controller.cs
+++ BASILISK/Assets/Scripts/Game_Controller.cs
@@ -1,6 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
 
 public class Game_Controller : MonoBehaviour {
 	public static Game_Controller control;
@@ -8,24 +13,25 @@ public class Game_Controller : MonoBehaviour {
     public bool paused;
     private GameObject pauseCanvas;
 	private Scene CurrentLevel;
+    private int level;
+	public float number;
 
 	// Use this for initialization
-	void Awake(){
-		if (control == null) 
-		{
-			DontDestroyOnLoad (gameObject);
-			control = this;
-		}
-		else if(control != this)
-		{
-			Destroy (gameObject);
-		}
-	}
+	//void Awake(){
+		//if (control == null) 
+		//{
+			//DontDestroyOnLoad (gameObject);
+			//control = this;
+		//}
+		//else if(control != this)
+		//{
+			//Destroy (gameObject);
+		//}
+	//}
 	void Start () {
+        level = 0;
 		CurrentLevel = SceneManager.GetActiveScene();
         paused = false;
-		Journals2 journals_script = GameObject.Find("Journals").GetComponent<Journals2>();
-		//journals_script = journals_script.collected<>();
 		CurrentLevel = SceneManager.GetActiveScene();
         pauseCanvas = transform.GetChild(0).gameObject;
 
@@ -33,6 +39,13 @@ public class Game_Controller : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        level = SceneManager.GetActiveScene().buildIndex;
+		if (Input.GetKeyDown (KeyCode.K)) {
+			SaveGame ();
+		}
+		if (Input.GetKeyDown (KeyCode.L)) {
+			LoadGame ();
+		}
         playerPauseGame();
         if (Input.GetKeyDown(KeyCode.R))
             restartLevel();
@@ -41,7 +54,7 @@ public class Game_Controller : MonoBehaviour {
 
     public void restartLevel() {
 		CurrentLevel = SceneManager.GetActiveScene();
-		SceneManager.LoadScene(CurrentLevel.name);
+		SceneManager.LoadScene(level);
 
     }
 
@@ -53,11 +66,33 @@ public class Game_Controller : MonoBehaviour {
     }
 	public void SaveGame()
 	{
-	
+		Journals2 journals_script = GameObject.Find("Journals").GetComponent<Journals2>();
+		BinaryFormatter bf = new BinaryFormatter();
+
+		FileStream save = File.Open (Application.persistentDataPath + "/save.dat", FileMode.Open);
+		Saves data = new Saves();
+
+		data.lvl_id = level;
+		//data.level = CurrentLevel;
+
+		bf.Serialize (save, data);
+		save.Close ();
 	}
-	public void RestartGame ()
+	public void LoadGame ()
 	{	
+		if (File.Exists (Application.persistentDataPath + "/save.dat")) {
+			FileStream save = File.Open(Application.persistentDataPath + "/save.dat",FileMode.Open);
+            BinaryFormatter bf = new BinaryFormatter();
+            Saves data = (Saves)bf.Deserialize(save);
+			save.Close ();
+			level = data.lvl_id;
+		//	CurrentLevel = data.level;
 		
+		} 
+		else 
+		{
+			print ("does not exist");
+		}
 	}
 
     private void runGame() {
@@ -68,5 +103,14 @@ public class Game_Controller : MonoBehaviour {
             Time.timeScale = 1;
         }
     }
+	[Serializable]
+	class Saves
+	{
+
+		public int lvl_id;
+        public List<GameObject> collected;
+        //public Object level;
+    }
+
 
 }
