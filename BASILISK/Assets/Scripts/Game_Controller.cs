7765cae2b9ce2f1b92559f94b4d5818794cf4531
Carnegie-senpai <sawyer3nick@gmail.com>
2016-11-20 20:58:59 -0800

    Saves journals
    Journals now save, but the references in the inspector are lost when a
    new scene is loaded.

--- BASILISK/Assets/Scripts/Game_Controller.cs
+++ BASILISK/Assets/Scripts/Game_Controller.cs
@@ -7,77 +7,82 @@ using System.IO;
 using System.Linq;
 using System.Collections.Generic;
 
-public class Game_Controller : MonoBehaviour {
-	public static Game_Controller control;
-//    public string pauseButton = "escape";
+public class Game_Controller : MonoBehaviour
+{
+    public static Game_Controller control;
+    //    public string pauseButton = "escape";
     public bool paused;
     private GameObject pauseCanvas;
-	private Scene CurrentLevel;
+    private Scene CurrentLevel;
     private int level;
 
     public int Journal_count;
     public GameObject[] journals;
     public GameObject player;
-    public List<GameObject> collected;
-    public int number;
-    public bool contains_journal;
+    public List<int> collected;
 
+    public bool contains_journal;
     // Use this for initialization
-    //void Awake(){
-    //if (control == null) 
-    //{
-    //DontDestroyOnLoad (gameObject);
-    //control = this;
-    //}
-    //else if(control != this)
-    //{
-    //Destroy (gameObject);
-    //}
-    //}
-    void Start () {
-        print(Application.persistentDataPath);
-        print(SceneManager.GetActiveScene().buildIndex);
-        level = 0;
-		CurrentLevel = SceneManager.GetActiveScene();
+    void Awake()
+    {
+        if (control == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            control = this;
+        }
+        else if (control != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+
+        //print(Application.persistentDataPath);
+        //print(SceneManager.GetActiveScene().buildIndex);
         paused = false;
-		CurrentLevel = SceneManager.GetActiveScene();
         pauseCanvas = transform.GetChild(0).gameObject;
+        collected = collected;
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-       // if (Input.GetKeyDown(KeyCode.Alpha1))
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (Input.GetKeyDown(KeyCode.Alpha1))
         //{
         //    collected.Remove(journals[0]);
         //}
         //else if (Input.GetKeyDown(KeyCode.Alpha2))
-       // {
-         //   collected.Remove(journals[1]);
+        // {
+        //   collected.Remove(journals[1]);
         //}
         //else if (Input.GetKeyDown(KeyCode.Alpha3))
         //{
-         //   collected.Remove(journals[2]);
+        //   collected.Remove(journals[2]);
         //}
         //else
         //{
-            //print("Invalid input");
+        //print("Invalid input");
         //}
         level = SceneManager.GetActiveScene().buildIndex;
-		if (Input.GetKeyDown (KeyCode.I)) {
-			SaveGame ();
-            print("saved");
-		}
-		if (Input.GetKeyDown (KeyCode.O)) {
-            print("loaded");
-            LoadGame ();
-            restartLevel();
-		}
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            SaveGame();
+            //print("saved");
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            LoadGame();
+            //print("loaded");
+        }
+        if (Input.GetKeyDown(KeyCode.Y)) { foreach (int thing in collected) { print(thing); } }
         playerPauseGame();
         if (Input.GetKeyDown(KeyCode.R))
             restartLevel();
         runGame();
-	}
+    }
     public void PickUp()
     {
 
@@ -87,65 +92,76 @@ public class Game_Controller : MonoBehaviour {
             var distance_to_journal = Vector3.Distance(player.transform.position, journal.transform.position);
             if (distance_to_journal < 2)
             {
-                print("about to add journal");
-                collected.Add(journal);
-                print("added journal");
-                collected = collected.OrderBy(tile => tile.name).ToList();
+                collected.Add(int.Parse(journal.name));
+                collected = collected.OrderBy(tile => tile).ToList();
             }
 
         }
         //Removes specific journal based on which key is pressed, will change to display journal later
 
     }
-    public void restartLevel() {
-		SceneManager.LoadScene(level);
+    public void restartLevel()
+    {
+        SceneManager.LoadScene(level);
 
     }
-
-    private void playerPauseGame() {
-        if (Input.GetKeyDown(KeyCode.Escape)) {
+    IEnumerator wait()
+    {
+        print("startedwait");
+        yield return new WaitForSeconds(5);
+        print("waited");
+    }
+    private void playerPauseGame()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
             paused = !paused;
             pauseCanvas.SetActive(!pauseCanvas.activeSelf);
         }
     }
-	public void SaveGame()
-	{
-		Journals2 journals_script = GameObject.Find("Journals").GetComponent<Journals2>();
-		BinaryFormatter bf = new BinaryFormatter();
+    public void SaveGame()
+    {
+        Journals2 journals_script = GameObject.Find("Journals").GetComponent<Journals2>();
+        BinaryFormatter bf = new BinaryFormatter();
 
-		FileStream save = File.Open (Application.persistentDataPath + "/save.dat", FileMode.Open);
-		Saves data = new Saves();
+        FileStream save = File.Open(Application.persistentDataPath + "/save.dat", FileMode.Open);
+        Saves data = new Saves();
         //Journals2 jc = GameObject.Find("Journals").GetComponent<Journals2>();
         //data.collected = jc.collected;
         data.lvl_id = level;
         data.collected = collected;
-		bf.Serialize (save, data);
-		save.Close ();
-	}
-	public void LoadGame ()
-	{	
-		if (File.Exists (Application.persistentDataPath + "/save.dat")) {
-			FileStream save = File.Open(Application.persistentDataPath + "/save.dat",FileMode.Open);
+        bf.Serialize(save, data);
+        save.Close();
+    }
+    public void LoadGame()
+    {
+        if (File.Exists(Application.persistentDataPath + "/save.dat"))
+        {
+            FileStream save = File.Open(Application.persistentDataPath + "/save.dat", FileMode.Open);
             BinaryFormatter bf = new BinaryFormatter();
             Saves data = (Saves)bf.Deserialize(save);
-			save.Close ();
-			level = data.lvl_id;
+            save.Close();
+            level = data.lvl_id;
+            restartLevel();
+            StartCoroutine(wait());
             collected = data.collected;
-            //Journals2 jc = GameObject.Find("Journals").GetComponent<Journals2>();
-            //jc.collected = data.collected;
-		
-		} 
-		else 
-		{
-			print ("does not exist");
-		}
-	}
 
-    private void runGame() {
-        if (paused && Time.timeScale != 0) {
+
+        }
+        else
+        {
+            print("does not exist");
+        }
+    }
+
+    private void runGame()
+    {
+        if (paused && Time.timeScale != 0)
+        {
             Time.timeScale = 0;
         }
-        else if (!paused && Time.timeScale == 0) {
+        else if (!paused && Time.timeScale == 0)
+        {
             Time.timeScale = 1;
         }
     }
@@ -160,13 +176,15 @@ public class Game_Controller : MonoBehaviour {
     }
 
     [Serializable]
-	class Saves
-	{
+    class Saves
+    {
 
-		public int lvl_id;
-        public List<GameObject> collected;
-        //public Object level;
+        public int lvl_id;
+        public List<int> collected;
     }
 
 
 }
+
+// Use this for initialization
+
